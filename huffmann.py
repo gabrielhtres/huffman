bcfb380b1e8c167a23d493c45790e2b90055c54f
AnaSitec001 <analaura@sitecbr.com.br>
2022-07-04 00:09:54 -0300

    Função principal finalizada

---
+++ huffmann.py
@@ -0,0 +1,42 @@
+Q = [['a', 45], ['b', 13], ['c', 12], ['d', 16], ['e', 9], ['f', 5]]
+C = []
+n = len(Q)
+
+def extrairMinimo(Q):
+    i = 0
+    menor = 99999
+    for no in Q:
+        if no[1] < menor:
+            menor = no[1]
+    
+    # print(menor)
+    while i < len(Q):
+        if Q[i][1] == menor:
+            retirado = Q.pop(i)
+            break
+        else:
+            i+=1
+    
+    return retirado
+
+def huffmann():
+    for j in range(0, n - 1):
+        esquerdo = extrairMinimo(Q)
+        direito = extrairMinimo(Q)
+        frequencia = esquerdo[1] + direito[1]
+        Z = [[esquerdo, direito], frequencia]
+        # print(Z)
+        Q.append(Z)
+        C.append(Z)
+
+huffmann()
+
+# print(Q)
+
+for i in C:
+    print(i)
+    for j in i:
+        print('Esquerdo')
+        print(j[0])
+        print('Direito')
+        print(j[1])
